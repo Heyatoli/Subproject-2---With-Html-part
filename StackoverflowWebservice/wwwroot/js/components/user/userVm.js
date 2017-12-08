c604a9c7a26abc4128be055e5400e00832e4c386
simon.rove.jensen <simon.rove.jensen@gmail.com>
2017-12-08 14:28:01 +0100

    added folders, renamed some shit, deleted some other shit, switch works

---
+++ StackoverflowWebservice/wwwroot/js/components/user/userVm.js
@@ -0,0 +1,189 @@
+﻿define(['knockout', 'postman', 'webservice'], function (ko, postman, webservice) {
+
+    return function (params) {
+
+        var title = ko.observable("noget andet!");
+
+        var tag = ko.observable("");
+
+        var displayingTagPosts = false;
+
+        var specificPostTitle = ko.observable("");
+        var specificPostBody = ko.observable("");
+
+        var links = ko.observableArray([]);    // Initially an empty array
+
+        var bodyTextDiv = ko.observable(false);
+
+        var showComments = ko.observable(false);
+        var currentPostComments = ko.observableArray([]);
+        var currentPostLink = ko.observable("");
+
+        var next = ko.observable();
+        var prev = ko.observable();
+
+        var showNext = ko.observable(false);
+        var showPrev = ko.observable(false);
+
+        var showBody = function () {
+            console.log(bodyTextDiv())
+            if (bodyTextDiv() == false) {
+                bodyTextDiv(true);
+            }
+            else {
+                bodyTextDiv(false);
+            }
+        }
+
+        var getNext = function () {
+            getLinks(next);
+        }
+
+        var getPrev = function () {
+            getLinks(prev);
+        }
+
+        var getPostWithTag = function () {
+            console.log(tag());
+
+            var myUrl = "http://localhost:5001/api/posts/tag/" + tag();
+            links.removeAll();
+
+            $.ajax({
+                url: myUrl,
+                type: "GET",
+                crossDomain: true,
+                dataType: 'json',
+                async: true,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    console.log(data);
+
+                    for (i = 0; i < data.data.length; i++) {
+                        links.push(data.data[i]);
+                    }
+                    console.log(data);
+                    next = data.next;
+                    prev = data.prev;
+                    displayNextPrev(data.next, data.prev);
+
+                }
+            });
+
+        }
+
+        var getPost = function (myUrl) {
+
+            //Calling function from Webservice
+
+            var cb = function (data) {
+                console.log(data);
+                specificPostTitle(data[0].title);
+                specificPostBody(data[0].body);
+                currentPostLink(data[0].link);
+            };
+
+            webservice.getPost(myUrl, cb);
+
+
+
+            showComments(false);
+
+        }
+
+        var getComments = function () {
+            currentPostComments.removeAll();
+            $.ajax({
+                url: currentPostLink(),
+                type: "GET",
+                crossDomain: true,
+                dataType: 'json',
+                async: true,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    for (i = 0; i < data.comments.length; i++) {
+                        currentPostComments.push(data.comments[i]);
+                    }
+                    showComments(true);
+                    console.log(currentPostComments());
+                }
+            });
+        }
+
+        var getLinks = function (url) {
+
+            links.removeAll();
+
+            var myUrl = url;
+
+            if (url == null) {
+                myUrl = "http://localhost:5001/api/posts";
+            }
+
+            $.ajax({
+                url: myUrl,
+                type: "GET",
+                crossDomain: true,
+                dataType: 'json',
+                async: true,
+                processData: false,
+                cache: false,
+                success: function (data) {
+                    console.log(data.data[0].link);
+                    for (i = 0; i < data.data.length; i++) {
+                        links.push(data.data[i]);
+                    }
+                    console.log(data);
+                    next = data.next;
+                    prev = data.prev;
+                    displayNextPrev(data.next, data.prev);
+                }
+            });
+        };
+
+        var displayNextPrev = function (next, prev) {
+
+            if (next != null) {
+                showNext(true);
+            }
+            else {
+                showNext(false);
+            }
+
+            if (prev != null) {
+                showPrev(true);
+            }
+            else {
+                showPrev(false);
+            }
+
+        }
+
+        return {
+            links,
+            getPostWithTag,
+            tag,
+            specificPostTitle,
+            bodyTextDiv,
+            getComments,
+            specificPostBody,
+            showComments,
+            currentPostComments,
+            showBody,
+            getPost,
+            getNext,
+            getPrev,
+            title,
+            next,
+            prev,
+            showNext,
+            showPrev,
+            getLinks,
+            displayNextPrev
+
+        };
+
+    }
+});
--- StackoverflowWebservice/wwwroot/js/main.js
+++ StackoverflowWebservice/wwwroot/js/main.js
@@ -1,6 +1,4 @@
-﻿
-
-require.config({
+﻿require.config({
 
     baseUrl: "js",
 
@@ -14,33 +12,26 @@ require.config({
 });
 
 require(['knockout'], function (ko) {
-
-    ko.components.register("mylist", {
-        viewModel: { require: "components/mylist/mylist" },
-        template: { require: "text!components/mylist/mylist_view.html"}
+    ko.components.register("post", {
+        viewModel: { require: "components/post/postVm" },
+        template: { require: "text!components/post/post.html" }
     });
 
-    ko.components.register("my-element", {
-        viewModel: { require: "components/element/element" },
-        template: { require: "text!components/element/element_view.html" }
+    ko.components.register("user", {
+        viewModel: { require: "components/user/userVm" },
+        template: { require: "text!components/user/user.html" }
     });
-
-    ko.components.register("index", {
-        viewModel: { require: "components/index/indexVm" },
-        template: { require: "text!components/index/index.html" }
-    });
-
 });
 
 require(['knockout', 'postman'], function(ko, postman) {
     var vm = (function() {
-        var currentView = ko.observable('index');
+        var currentView = ko.observable('post');
         var currentParams = ko.observable("!!!");
-        var switchComponent = function() {
-            if (currentView() === "mylist") {
-                currentView("my-element");
+        var switchComponent = function () {
+            if (currentView() === "post") {
+                currentView("user");
             } else {
-                currentView("mylist");
+                currentView("post");
             }
 
         }
